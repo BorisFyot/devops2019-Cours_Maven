d0983fa2abd62aa663d3de39385e7e7453a6f7c6
root <root@localhost.localdomain>
2019-04-16 07:26:33 -0700

    test sur maven

---
+++ maven/maven/src/test/java/mymaven/app/AppTest.java
@@ -0,0 +1,29 @@
+package mymaven.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test() {
+	int a =1;
+	int b =1;
+        // assert statements
+        assertEquals(a, b);
+    }
+/**    @Test
+    public void test2() {
+	int a =1;
+	int b =2;
+        // assert statements
+        assertEquals(a, b);
+    } */
+}
--- tp5/projetmaven/src/test/java/com/example/apiwebapp/ApiWebappApplicationTests.java
+++ tp5/projetmaven/src/test/java/com/example/apiwebapp/ApiWebappApplicationTests.java
@@ -1,16 +1,19 @@
 package com.example.apiwebapp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+
 public class ApiWebappApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        MyClass tester = new MyClass(); // MyClass is tested
 
+        // assert statements
+        assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
+        assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
+        assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+    }
 }
+
